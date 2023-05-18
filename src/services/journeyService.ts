ba0dc2c3534e1f33acc2dd7737ab010fed4608bf
Mei <meysunlee@gmail.com>
2023-05-18 18:23:59 +0300

    edit endpoints

--- src/services/journeyService.ts
+++ src/services/journeyService.ts
@@ -1,3 +1,4 @@
+import { ILike } from "typeorm/find-options/operator/ILike";
 import { AppDataSource } from "../entity/data-source";
 import { Journey } from "../entity/Journey";
 
@@ -19,29 +20,21 @@ export type IJouney = {
 
 const getAllJourneys = async (pageSize: number, pageNumber: number, search: string | undefined): Promise<IJouney | Journey[]> => {
     const offset = pageSize * pageNumber
-    const limit = pageSize * (pageNumber + 1)
+    const limit = pageSize
 
-    if (search !== '' && search !== undefined) {
+    console.log(search)
+    if (search && search !== 'undefined') {
+        console.log('at search')
         const [filteredJourneys, count] = await journeyData
-        .createQueryBuilder("journey")
-        .where("journey.departurestation_name LIKE :search OR journey.returnstation_name LIKE :search", { search: `%${search}%` })
-        .skip(offset)
-        .limit(limit)
-  
-        .getRawMany()
-        /*const [filteredJourneys, count] = await journeyData
             .findAndCount({
                 where: [{
                     departurestation_name: ILike(`%${search}%`),
                 }, {
                     returnstation_name: ILike(`%${search}%`),
                 }],
-                order: {
-                    [sort.key]: sort.value
-                },
                 skip: offset,
                 take: limit,
-            })*/
+            })
         const result = { journeyList: filteredJourneys, total: count }
         return result
     }
